5f636264ab0fb034faf57de4aea68f5eb796de33
Deep Patil <59699631+patildeep07@users.noreply.github.com>
2024-04-15 16:54:39 +0530

    feat - UI of Signup page
    - Created UI components of the signup page

--- chartify/src/pages/Signup/Signup.jsx
+++ chartify/src/pages/Signup/Signup.jsx
@@ -1,9 +1,100 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
+  // Logic
+  const [userDetails, setUserDetails] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const signupHandler = () => {
+    console.log({ userDetails });
+    const signupMail = userDetails.email.replace(/\s/g, "");
+    const signupPassword = userDetails.password.replace(/\s/g, "");
+    const signupDetails = {
+      name: userDetails.name,
+      email: signupMail,
+      password: signupPassword,
+    };
+
+    console.log({ signupDetails });
+  };
+
+  // UI
   return (
     <div>
-      <h1>Signup</h1>
+      <div className="mx-auto my-10 flex min-h-fit max-w-[90%] flex-col items-start justify-start gap-5 rounded-lg border border-gray-400 px-10 py-7 sm:max-w-fit">
+        <h1 className="self-center text-2xl font-bold">Create your account</h1>
+
+        <div className="flex flex-col  self-center ">
+          <h2 className=" self-center text-xl font-semibold ">
+            Welcome to Chartify
+          </h2>
+        </div>
+
+        <div className="flex flex-col w-full">
+          <label htmlFor="name">Name:</label>
+          <input
+            id="name"
+            type="text"
+            placeholder="Enter"
+            value={userDetails.name}
+            onChange={(e) => {
+              setUserDetails({ ...userDetails, name: e.target.value.trim() });
+            }}
+            className="my-1 w-full  sm:w-[35vw] sm:min-w-[25vw] sm:max-w-[80vw] rounded-md border border-gray-400 px-2 py-1"
+          />
+        </div>
+
+        <div className="flex flex-col w-full">
+          <label htmlFor="email">Email:</label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Enter"
+            value={userDetails.email}
+            onChange={(e) => {
+              setUserDetails({ ...userDetails, email: e.target.value });
+            }}
+            className="my-1 w-full  sm:w-[35vw] sm:min-w-[25vw] sm:max-w-[80vw] rounded-md border border-gray-400 px-2 py-1"
+          />
+        </div>
+
+        <div className="flex flex-col w-full">
+          <label htmlFor="password">Password:</label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Enter"
+            value={userDetails.password}
+            onChange={(e) => {
+              setUserDetails({
+                ...userDetails,
+                password: e.target.value,
+              });
+            }}
+            className="my-1 w-full sm:w-[35vw] sm:min-w-[25vw] sm:max-w-[80vw] rounded-md border border-gray-400 px-2 py-1"
+          />
+        </div>
+
+        <button
+          onClick={signupHandler}
+          className="w-full self-center rounded-md bg-black px-5 py-2 tracking-wider text-white"
+        >
+          Sign up
+        </button>
+
+        <div className="border-gray-500-900 w-full border"></div>
+
+        <p className="self-center text-gray-600">
+          Already have an account?{" "}
+          <span className="mx-1 cursor-pointer font-semibold text-black">
+            <Link to={"/login"}>Login</Link>
+          </span>
+        </p>
+      </div>
     </div>
   );
 };
